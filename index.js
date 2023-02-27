1e03009ba385ef00aa26d0d10e24eafcf6def43f
bairamov <ellezbayramov2004@gmail.com>
2023-02-27 18:54:56 +0400

    Added Draw Type

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-///TODO FIX WIDTH AND HEIGHT!
+let ClearButton = document.getElementById("ClearButton");
+let DrawButton = document.getElementById("DrawButton");
 
 let c = document.getElementById("myCanvas");
 let ctx = c.getContext("2d");
+
+let DrawType = {
+    aInternal: "",
+    aListener: function(val) {},
+    set a(val) {
+        this.aInternal = val;
+        this.aListener(val);
+    },
+    get a() {
+        return this.aInternal;
+    },
+    registerListener: function(listener) {
+        this.aListener = listener;
+    }
+};
+
 c.width = window.innerWidth;
 c.height = window.innerHeight;
 
@@ -12,7 +29,22 @@ window.addEventListener("resize",function () {
 
 let coordinates = [];
 
-c.addEventListener("mousemove",function(event) {
+ClearButton.addEventListener("click",function (event) {
+    ctx.clearRect(0, 0, c.width, c.height);
+})
+
+DrawButton.addEventListener("click",function (event) {
+    console.log("Type: " + DrawType.a);
+    if(DrawType.a == "Draw")
+    {
+        DrawType.a = "";
+    }else{
+        DrawType.a = "Draw";
+    }
+})
+
+function OpenDrawLine(event)
+{
     const currentCoordinate = {"x" : event.x,"y" : event.y};
     coordinates.push(currentCoordinate);
     const lastCoordinate = coordinates.length - 1;
@@ -26,6 +58,29 @@ c.addEventListener("mousemove",function(event) {
         ctx.strokeStyle = "#32a852"
         ctx.stroke();
     }
+}
+
+DrawType.registerListener(function (val) {
+    switch (val) {
+        case "Draw" :  {
+            c.addEventListener("mousemove",function(event) {
+                const currentCoordinate = {"x" : event.x,"y" : event.y};
+                coordinates.push(currentCoordinate);
+                const lastCoordinate = coordinates.length - 1;
+                if(typeof coordinates[lastCoordinate - 1] != "undefined")
+                {
+                    const startCoordinate = coordinates[lastCoordinate - 1];
+                    const toCoordinate = coordinates[lastCoordinate];
+                    ctx.beginPath();
+                    ctx.moveTo(startCoordinate.x,startCoordinate.y);
+                    ctx.lineTo(toCoordinate.x,toCoordinate.y);
+                    ctx.strokeStyle = "#32a852"
+                    ctx.stroke();
+                }
+            })
+            break;
+        }default : {
+            c.removeEventListener("mousedown", handleMouseDown, true); // Succeeds
+        }
+    }
 })
-
-
